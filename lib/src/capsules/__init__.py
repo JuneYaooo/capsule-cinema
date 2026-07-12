261fed7ee225459b9e11ad3dc8d134c253b5f437
june <june2@JunedeMac-mini.local>
2026-07-12 08:20:44 -0700

    docs: publish capsule production core contracts

--- lib/src/capsules/__init__.py
+++ lib/src/capsules/__init__.py
@@ -8,8 +8,36 @@ from src.capsules.model import (
     CapsuleMatch,
     CapsuleMetadata,
     CapsulePromise,
+    CapsuleReadOrder,
     CapsuleRunner,
 )
+from src.capsules.effect import (
+    ArtifactEvidence,
+    EffectCheck,
+    EffectReport,
+    build_effect_report,
+)
+from src.capsules.instance import CapsuleInstance, configure_instance, write_instance
+from src.capsules.preservation import (
+    PackageSnapshot,
+    PreservationDisposition,
+    PreservationManifest,
+    SectionRecord,
+    build_preservation_manifest,
+    inventory_sections,
+    snapshot_package,
+    validate_preservation_manifest,
+)
+from src.capsules.production import (
+    EvidenceRequirement,
+    Objective,
+    ProductionPlan,
+    ProductionStep,
+    QualityRequirement,
+    build_production_plan,
+    production_plan_digest,
+)
+from src.capsules.reading import STAGES, StageName, load_stage_resources
 from src.capsules.result import Issue, ResultEnvelope, failure, success
 
 __all__ = [
@@ -20,7 +48,33 @@ __all__ = [
     "CapsuleMatch",
     "CapsuleMetadata",
     "CapsulePromise",
+    "CapsuleReadOrder",
     "CapsuleRunner",
+    "CapsuleInstance",
+    "configure_instance",
+    "write_instance",
+    "PackageSnapshot",
+    "PreservationDisposition",
+    "PreservationManifest",
+    "SectionRecord",
+    "build_preservation_manifest",
+    "inventory_sections",
+    "snapshot_package",
+    "validate_preservation_manifest",
+    "STAGES",
+    "StageName",
+    "load_stage_resources",
+    "Objective",
+    "EvidenceRequirement",
+    "QualityRequirement",
+    "ProductionStep",
+    "ProductionPlan",
+    "build_production_plan",
+    "production_plan_digest",
+    "ArtifactEvidence",
+    "EffectCheck",
+    "EffectReport",
+    "build_effect_report",
     "Issue",
     "ResultEnvelope",
     "failure",
